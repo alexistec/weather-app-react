2ebd8a1dfbf5f244d425c1a678d32057c26573f5
Alexis quionez <alexisquionez21@gmail.com>
2021-09-01 18:22:29 -0300

    DOCS are added for applicaction deploy

---
+++ docs/precache-manifest.3edc8034092802a2909202c9ba203be4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa2a0eaf20b9f646518cc1aabbddb53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93e5ca6655094df2e49a",
+    "url": "/static/css/main.733a75cf.chunk.css"
+  },
+  {
+    "revision": "6c0f216a23bb5642f6db",
+    "url": "/static/js/2.ea2a016e.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.ea2a016e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e5ca6655094df2e49a",
+    "url": "/static/js/main.a2293d9c.chunk.js"
+  },
+  {
+    "revision": "d7e9bb608f892a3ebf30",
+    "url": "/static/js/runtime-main.2ad375de.js"
+  }
+]);
